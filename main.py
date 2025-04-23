56750f52f1bdd8a60961db01b37f223b14a448d2
alek <krasnobaev.aleksii@gmail.com>
2025-04-23 09:18:28 +0500

    First change

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ class Student:
         self.grades = {} 
 
     # Функция считает среднюю оценку студента за домашнее задание 
-    def avr_grade(self):
+    def average_grade(self):
         summa = 0
         count = 0 
         for i in self.grades:         
             summa += (sum(self.grades[i]))
-            count += 1
+            count += len(self.grades[i])
         res = summa/count
         return res
 
     # выставление оценок лекторам
-    def lecturer_grades(self, lecturer, course, grade):
+    def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades and 1 <= grade <= 10 :
                 lecturer.grades[course] += [grade]
@@ -30,10 +30,10 @@ class Student:
     # Перегрузка метода __str__ для студента  
     def __str__(self):
         
-        return f" Имя: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за лекции: {self.avr_grade()} \n Курсы в процессе изучения: {", ".join(self.courses_in_progress)} \n Завершенные курсы :{", ".join(self.finished_courses)}"
+        return f" Имя: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за лекции: {self.average_grade()} \n Курсы в процессе изучения: {", ".join(self.courses_in_progress)} \n Завершенные курсы :{", ".join(self.finished_courses)}"
     # Перегрузка оператора РАВЕНСТВО для студента
     def __eq__(self, other):
-        return self.avr_grade() == other.avr_grade()   
+        return self.average_grade() == other.average_grade()   
      
 class Mentor:
     def __init__(self, name, surname):
@@ -44,20 +44,20 @@ class Mentor:
 
 class Lecturer(Mentor):
     # Функция считает среднюю оценку лектора за лекции
-    def avr_grade(self):
+    def average_grade(self):
         summa = 0
         count = 0 
         for i in self.grades:         
             summa += (sum(self.grades[i]))
-            count += 1
+            count += len(self.grades[i])
         res = summa/count
         return res
     # Перегрузка метода __str__ для лектора
     def __str__(self):
-        return f" Имя: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за лекции: {self.avr_grade()}"
+        return f" Имя: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за лекции: {self.average_grade()}"
     # Перегрузка оператора РАВЕНСТВО для лектора
     def __eq__(self, other):
-        return self.avr_grade() == other.avr_grade()
+        return self.average_grade() == other.average_grade()
     
 class Reviewer(Mentor):
     # выставление оценок студентам
@@ -73,8 +73,8 @@ class Reviewer(Mentor):
         return f" Имя: {self.name} \n Фамилия: {self.surname}"
     
 
-reiewer_1 = Reviewer("Bill", "Geits")
-reiewer_2 = Reviewer("Philip", "Moris")
+reviewer_1 = Reviewer("Bill", "Geits")
+reviewer_2 = Reviewer("Philip", "Moris")
 
 lecture_1 = Lecturer("Boby", "Tarantino")
 lecture_2 = Lecturer("Jacky", "Chan")
@@ -82,33 +82,33 @@ lecture_2 = Lecturer("Jacky", "Chan")
 student_1 = Student("Stiv", "Djobs", "man")
 student_2 = Student("Maria", "Gonsales", "woman")
 
-reiewer_1.courses_attached.append("Python - начало")   
-lecture_1.courses_attached.append("Python - начало")
-student_1.courses_in_progress.append("Python - начало")
-student_1.finished_courses.append("Python - start")
-student_1.finished_courses.append("Hello word!!!")
+reviewer_1.courses_attached.append("Python")   
+lecture_1.courses_attached.append("Python")
+student_1.courses_in_progress.append("Python")
+student_1.finished_courses.append("Git")
+student_1.finished_courses.append("Word")
 
-student_1.lecturer_grades(lecture_1, "Python - начало", 5)
-reiewer_1.rate_hw(student_1,"Python - начало", 5) 
-reiewer_1.rate_hw(student_1,"Python - начало", 8) 
+student_1.rate_lecturer(lecture_1, "Python", 5)
+reviewer_1.rate_hw(student_1,"Python", 5) 
+reviewer_1.rate_hw(student_1,"Python", 8) 
 
 # подсчет средней оценки по всем студентам  врамках одного курса
-def total_avg_of_course(student_list, course_name):
+def total_student_avg_for_course(student_list, course_name):
     for i in student_list:
         summa = 0
         number_grades = 0
-        if i.grades.key == course_name:
+        if course_name in i.grades:
             summa += sum(i.grades[course_name])
             number_grades += len(i.grades[course_name])
     return summa/number_grades
 
 # подсчет средней оценки по всем лекторам в рамках одного курса
 
-def total_avg_of_course(lector_list, course_name):
+def total_lecturer_avg_for_course(lector_list, course_name):
     for i in lector_list:
         summa = 0
         number_grades = 0
-        if i.grades.key == course_name:
+        if course_name in i.grades:
             summa += sum(i.grades[course_name])
             number_grades += len(i.grades[course_name])
     return summa/number_grades
